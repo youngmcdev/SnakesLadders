178b18b22e052eeac69527266dda4d6f6c0433c9
Mark <mark.young@cuanswers.com>
2020-01-11 18:44:54 -0500

    Beginning of this repo. New solution.

---
+++ SnakesLadders/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakesLadders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
