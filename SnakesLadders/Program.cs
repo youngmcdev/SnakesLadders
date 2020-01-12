cb4a9c44a01175089085fc7bccb17ed652861644
Mark <mark.young@cuanswers.com>
2020-01-12 12:37:07 -0500

    Driver program for game board

--- SnakesLadders/Program.cs
+++ SnakesLadders/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using SnakesLadders.Backend;
+using System.Collections.Generic;
 
 namespace SnakesLadders
 {
     class Program
     {
+        private static readonly int _gameSize = 100;
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var svc = new GameBoardService();
+            var gameBoard = svc.InitializeGameBoard(_gameSize);
+            var substitutions = svc.GenerateGameSpaceSubstitutions(gameBoard, new List<(int, int)> 
+            { 
+                (4,14) 
+            });
+            svc.ApplyGameSpaceSubstitutions(gameBoard, substitutions);
+            int x = 0;
         }
     }
 }
